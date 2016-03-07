effbfc45cca7ee120bd06e1bd7660f3533f9ba07
Rob <rcarroll@colgate.edu>
2016-03-07 21:32:22 +0000

    Add user data to article listing page

--- db/migrate/20160302201001_add_description_to_articles.rb
+++ db/migrate/20160302201001_add_description_to_articles.rb
@@ -3,5 +3,6 @@ class AddDescriptionToArticles < ActiveRecord::Migration
     add_column :articles, :description, :text
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
+    add_column :articles, :user_id, :integer
   end
 end
