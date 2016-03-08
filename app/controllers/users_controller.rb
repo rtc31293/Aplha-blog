078a10864f69d4aceccc142c5019859ae76751d1
Rob <rcarroll@colgate.edu>
2016-03-08 14:42:34 +0000

    Edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,27 @@ class UsersController < ApplicationController
         end
     end
     
+    def edit
+        set_article
+    end
+    
+    def update
+        set_article
+        if @user.update(user_params)
+            flash[:success] = "Your Account was updated successfully!"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
     private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_article
+        @user = User.find(params[:id])
     end
     
 end
